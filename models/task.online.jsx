420d10b4c58104e2aae05d556300dd44bee1e735
Nawe <nahuelbrizu@outlook.com>
2022-05-18 18:52:47 -0300

    Update task.online.jsx

--- models/task.online.jsx
+++ models/task.online.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {TaskCompA} from "../../models/taskClass.class";
+import {TaskCompA} from "./taskClass.class";
 
 const TaskConnectComponent = ({ task }) => {
     return (
